76bcecdf8bc50d16584e3e1e367230073e15d0ee
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-08 19:27:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarf_names)
   # Your code here
   p dwarf_names.sort_by{|dwarf| dwarf.length}
 end
-roll_call_dwarves("Doc", "Dopey", "Bashful", "Grump")
+roll_call_dwarves("Doc", "Dopey", "Bashful", "Grumpy")
 
 def summon_captain_planet# code an argument here
   # Your code here
