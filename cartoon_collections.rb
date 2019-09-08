baf4cc912cb940056799f4eedfc459676b659d92
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-08 19:41:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(planeteer_calls)
   # Your code here
 end
 
-def long_planeteer_calls(calls)
+def long_planeteer_calls(array_of_calls)
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
