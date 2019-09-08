51f769f59b9d5583215a38b9d714e6da3e043141
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-08 19:33:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarf_names)
   # Your code here
-  dwarf_names.each_with_index{|item, index| p (index + 1), item}
+  dwarf_names.each_with_index{|item, index| p (index + 1) item}
 end
 
 def summon_captain_planet# code an argument here
