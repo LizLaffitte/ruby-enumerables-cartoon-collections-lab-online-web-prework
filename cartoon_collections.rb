1f7c281d8167a15783232bcbf8ba8ef41f4dd1cd
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-08 19:43:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,9 +8,9 @@ def summon_captain_planet(planeteer_calls)
   # Your code here
 end
 
-def long_planeteer_calls(array_of_calls)
+def long_planeteer_calls(array_calls)
   # Your code here
-  array_of_calls.any?{|call| call.length > 4}
+  array_calls.any?{|call| call.length > 4}
   
 end
 
